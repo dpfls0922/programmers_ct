9f6bc4c6809639996b19a9e33330608f79221919
dpfls0922 <dpfls0922@naver.com>
2024-11-01 17:17:57 +0900

    study: cj 최대이익

---
+++ 동적계획법(DP)/cj 최대이익.py
@@ -0,0 +1,31 @@
+def max_profit(costs, costX, costY):
+    rows, cols = len(costs), len(costs[0])
+    earn = [[0] * cols for _ in range(rows)]
+    earn[0][0] = costs[0][0]
+
+    for j in range(1, cols):
+        earn[0][j] = earn[0][j - 1] + costs[0][j] - costX
+
+    for i in range(1, rows):
+        earn[i][0] = earn[i - 1][0] + costs[i][0] - costY
+
+    # DP 테이블 채우기
+    for i in range(1, rows):
+        for j in range(1, cols):
+            # 오른쪽으로 이동하는 경우와 아래로 이동하는 경우 중 최대 이익 선택
+            earn[i][j] = max(
+                earn[i][j - 1] + costs[i][j] - costX,
+                earn[i - 1][j] + costs[i][j] - costY
+            )
+
+    return earn[-1][-1]
+
+costs = [
+    [5, 3, 2],
+    [1, 4, 1],
+    [1, 5, 3]
+]
+costX = 1
+costY = 2
+
+print(max_profit(costs, costX, costY))
