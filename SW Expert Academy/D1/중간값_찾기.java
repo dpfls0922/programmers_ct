11c5e27956dd5af6006d3f0958fd62898debc311
dpfls0922 <dpfls0922@naver.com>
2025-01-10 13:37:53 +0900

    study: 중간값 찾기

---
+++ SW Expert Academy/D1/중간값_찾기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 중간값_찾기 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] nums = new int[N];
+		
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int n = Integer.parseInt(st.nextToken());
+			nums[i] = n;
+		}
+		
+		Arrays.sort(nums);
+		int index = nums.length / 2;
+		
+		System.out.println(nums[index]);
+	} 
+}
